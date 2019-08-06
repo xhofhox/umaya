4866a4db3fa1dce96da5ad9dd986a2f74e906496
xhofhox <ing.g.adolfo@gmail.com>
2019-08-06 01:03:02 -0500

    eliminar

--- resources/assets/js/remote.js
+++
@@ -1,6 +0,0 @@
-$(document).on('ajax:success', function(e, xhr){
-    if(!$('#modal').length){
-        $('body').append($('<div class="modal" id="modal"></div>'))
-    }
-   $('#modal').html(xhr.responseText).modal('show');
-});
