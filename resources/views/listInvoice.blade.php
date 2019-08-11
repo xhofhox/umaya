4a796562b9e1dc5a30152dd5db854879371fadca
xr00t <ing.g.adolfo@gmail.com>
2019-08-11 18:17:22 -0500

    Listado CFDI
    Quitar Filtros obsoletos

--- resources/views/listInvoice.blade.php
+++ resources/views/listInvoice.blade.php
@@ -8,7 +8,7 @@
     <div class="col-md-12 col-md-offset-1">
       <div class="panel panel-default">
         <div class="panel-heading"><h3>Listado CFDI</h3></div>
-          <!-- Filtro-->
+          <!-- Filtro
           <div class="row">
             <form class="col s12">
               <div class="row">
@@ -29,7 +29,7 @@
               </div>
             </form>
           </div>
-               <!-- Filtro-->
+               Filtro-->
         
           <div class="panel-body">
             <form method="POST" 
@@ -37,7 +37,7 @@
                   accept-charset="UTF-8" 
                   id="list-invoice">
               <input type="hidden" name="_token" value="{{ csrf_token() }}">
-              <div class="form-group">
+            <!--  <div class="form-group">
                 <div class="col-md-6 hidden">
                   <label for="Servidor">Servidor:</label>
                   <select class="form-control col-md-8 selection-list hidden" name="Servidor">
@@ -50,7 +50,7 @@
                   <br />
                   <button id="show-list" type="submit" class="waves-effect waves-light btn-small cyan">Filtrar</button>
                 </div>
-              </div>
+              </div>-->
               @include('sweet::alert')              
             </form>
             <br/>
@@ -101,78 +101,6 @@
                 </tbody>
               </table>
 
-              <table id="example" class="display" style="width:100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Prescott Bartlett</td>
-                <td>Technical Author</td>
-                <td>London</td>
-                <td>27</td>
-                <td>2011/05/07</td>
-                <td>$145,000</td>
-            </tr>
-            <tr>
-                <td>Gavin Cortez</td>
-                <td>Team Leader</td>
-                <td>San Francisco</td>
-                <td>22</td>
-                <td>2008/10/26</td>
-                <td>$235,500</td>
-            </tr>
-            <tr>
-                <td>Martena Mccray</td>
-                <td>Post-Sales support</td>
-                <td>Edinburgh</td>
-                <td>46</td>
-                <td>2011/03/09</td>
-                <td>$324,050</td>
-            </tr>
-            <tr>
-                <td>Unity Butler</td>
-                <td>Marketing Designer</td>
-                <td>San Francisco</td>
-                <td>47</td>
-                <td>2009/12/09</td>
-                <td>$85,675</td>
-            </tr>
-            <tr>
-                <td>Howard Hatfield</td>
-                <td>Office Manager</td>
-                <td>San Francisco</td>
-                <td>51</td>
-                <td>2008/12/16</td>
-                <td>$164,500</td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-    </table>
           </div>
         </div>
     </div>
@@ -185,6 +113,6 @@
 
 <script>
   $(document).ready(function() {
-    $('#example').DataTable();
+    $('#table-records').DataTable();
   });
  </script>
