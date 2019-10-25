d1687b488f6523666fe6d03f9071cdfe1ffd2cff
xr00t <ing.g.adolfo@gmail.com>
2019-10-24 21:15:04 -0500

    Llenado modelo payRecipments

--- app/PayRecipments.php
+++ app/PayRecipments.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class PayRecipments extends Model
 {
     //
+        protected $table = "pay_recipments";
+
+    protected $fillable = array
+    (
+    'id',
+    'payment_id',
+    'to_pay',
+    'discount',
+    'scholarship_id',
+    'created_at',
+    'updated_at',
+    'scholarship_validation',
+    'invoiced',
+    'id_global_invoice' 
+    );	
+    
+    
 }
