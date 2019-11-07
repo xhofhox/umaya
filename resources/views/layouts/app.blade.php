a47e645f926dd28d99cd0d6f803cad1427e14afd
Nagisa <natty.teran@gmail.com>
2019-11-07 13:54:58 -0600

    Loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,6 @@
 
 		<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.6/dist/loadingoverlay.min.js" defer></script>
 
-		<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.6/dist/loadingoverlay.min.js"></script>
-
 		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
