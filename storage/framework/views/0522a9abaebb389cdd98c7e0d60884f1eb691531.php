11660ad57b4c45956f5de5c8db09524531ee02f1
Nagisa <natty.teran@gmail.com>
2019-11-07 16:01:23 -0600

    log

--- storage/framework/views/0522a9abaebb389cdd98c7e0d60884f1eb691531.php
+++ storage/framework/views/0522a9abaebb389cdd98c7e0d60884f1eb691531.php
@@ -1,80 +1,76 @@
 <!DOCTYPE html>
 <html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-    <title><?php echo e(config('app.name', 'Pagos')); ?></title>
+		<title><?php echo e(config('app.name', 'umaya')); ?></title>
 
-    <!-- Scripts -->
-    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
-    <script src="<?php echo e(asset('js/materialize.js')); ?>" defer></script>
+		<!-- Scripts -->
+		<script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+		<script src="<?php echo e(asset('js/materialize.js')); ?>" defer></script>
+		<script src="<?php echo e(asset('js/loadingoverlay.js')); ?>" defer></script>
+		<script src="<?php echo e(asset('js/loadingoverlay.min.js')); ?>" defer></script>
     
-    <script src="<?php echo e(asset('js/jquery-3.3.1.js')); ?>" defer></script>
-    <script src="<?php echo e(asset('js/jquery.dataTables.min.js')); ?>" defer></script>
-    
-    
-    <!--<script src="<?php echo e(asset('js/tooltipster.bundle.min.js')); ?>" defer></script>-->
-    
+		<script src="<?php echo e(asset('js/jquery-3.3.1.js')); ?>" defer></script>
+		<script src="<?php echo e(asset('js/jquery.dataTables.min.js')); ?>" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
 
-    <!-- Styles -->
-    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
-    <link href="<?php echo e(asset('css/materialize.css')); ?>" rel="stylesheet">
-    
-    <link href="<?php echo e(asset('css/jquery.dataTables.min.css')); ?>" rel="stylesheet">
-    
-   <!-- <link href="<?php echo e(asset('css/tooltipster.bundle.min.css')); ?>" rel="stylesheet">-->
-   
-    
-    
-    <!-- <link rel=”stylesheet” href=”css/sweetalert.css”>
-    <script src=”js/sweetalert.js”></script> -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <!--<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js"></script>-->
+		<script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.6/dist/loadingoverlay.min.js" defer></script>
 
+		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+		<script src="<?php echo e(asset('js/custom.js')); ?>"></script>
+
+		<!--<script src="<?php echo e(asset('js/tooltipster.bundle.min.js')); ?>" defer></script>-->
+
+		<!-- Fonts -->
+		<link rel="dns-prefetch" href="//fonts.gstatic.com" >
+		<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css" >
+
+		<!-- Styles -->
+		<link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet" >
+		<link href="<?php echo e(asset('css/materialize.css')); ?>" rel="stylesheet" >
     
-    <!--<script src="/js/materialize.js"></script>
-    <script src="/css/materialize.css"></script>
-       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
-            
+		<link href="<?php echo e(asset('css/jquery.dataTables.min.css')); ?>" rel="stylesheet" >
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-              <div class="navbar-header">
-                <a class="navbar-brand" href="#">Control Pagos</a>
-              </div>
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="http://localhost:8083/formulario">Pago por depósito</a></li>
-                <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Facturación
-                  <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="http://localhost:8083/invoice/create/0">Crear CFDI</a></li>
-                    <li><a href="http://localhost:8083/invoice/index">Listar CFDI's</a></li>
-                    <li><a href="http://localhost:8083/invoice/massive">Listar CFDI's Masivos</a></li>
-                  </ul>
-                </li>
-            </ul>
-        </div>
-        </nav>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" >
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" >
 
-        <main class="py-4">
-            <?php echo $__env->yieldContent('content'); ?>
-        </main>
-    </div>
-</body>
+	</head>
+	<body>
+		<div id="app">
+			<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+				  <div class="navbar-header">
+					<a class="navbar-brand" href="#">Control Pagos</a>
+				  </div>
+				  <ul class="nav navbar-nav">
+					<li class="active"><a href="<?php echo e('/formulario'); ?>">Pago por depósito</a></li>
+					<li class="dropdown">
+					  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Facturación
+					  <span class="caret"></span></a>
+					  <ul class="dropdown-menu">
+						<li><a href="<?php echo e('/invoice/index/1'); ?>">Listar CFDI's</a></li>
+						<li><a href="<?php echo e('/invoice/create/0'); ?>">Crear CFDI</a></li>
+						<li><a href="<?php echo e('/invoice/massive/1/0'); ?>">Crear CFDI's Masivos</a></li>
+						<li><a href="<?php echo e('/invoice/createGlobal/0'); ?>">Crear CFDI Global</a></li>
+					  </ul>
+					</li>
+					<li class="active"><a href="<?php echo e('/invoice/creditNote/0'); ?>">Nota de Crédito</a></li>
+				</ul>
+			</div>
+			</nav>
+
+			<main class="py-4">
+				<?php echo $__env->yieldContent('content'); ?>
+			</main>
+		</div>
+	</body>
 </html>
